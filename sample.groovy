1b5aaf6a59668aab3f0beff08eb57dbbcab25712
Mithunajha <89258602+Mithunajha@users.noreply.github.com>
2022-06-11 12:51:57 +0530

    Update sample.groovy

--- sample.groovy
+++ sample.groovy
@@ -4,14 +4,14 @@ pipeline {
         string(name: 'Name', defaultValue:'Mithunajha', description:'Enter Name')
         string(name: 'Roll No',defaultValue:'20CSR',description:'Enter Your Roll No')
         choice(name: 'Section', choices: ['A','B','C','D'],description:'Enter Section')
-        booleanParam(name: 'Placement Willingness', defaultValue: True,description:'Are you willing in placement side?')
-        choice(name: 'Area Of Interest', choices: ['Coding','Designing','Others'],description:'Enter Your Area Of Interest')
+        booleanParam(name: 'Placement_Willingness', defaultValue: True,description:'Are you willing in placement side?')
+        choice(name: 'Area_Of_Interest', choices: ['Coding','Designing','Others'],description:'Enter Your Area Of Interest')
     }
     stages {
         stage('Hello') {
             steps {
                 echo "Hello ${params.Name} and Your Roll No is ${params.Roll No} from ${params.Section}"
-                echo "Hope Your Area of Interest is ${params.Area Of Interest} and Your willingness is ${params.Placement Willingness}"
+                echo "Hope Your Area of Interest is ${params.Area_Of_Interest} and Your willingness is ${params.Placement_Willingness}"
             }
         }
     }
