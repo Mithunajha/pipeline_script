19954b4c882e2037f3a9761975be3de95b74c925
Mithunajha <89258602+Mithunajha@users.noreply.github.com>
2022-06-11 12:55:33 +0530

    Update sample.groovy

--- sample.groovy
+++ sample.groovy
@@ -4,7 +4,7 @@ pipeline {
         string(name: 'Name', defaultValue:'Mithunajha', description:'Enter Name')
         string(name: 'Roll_No',defaultValue:'20CSR',description:'Enter Your Roll No')
         choice(name: 'Section', choices: ['A','B','C','D'],description:'Enter Section')
-        booleanParam(name: 'Placement_Willingness', defaultValue: True,description:'Are you willing in placement side?')
+        booleanParam(name: 'Placement_Willingness', defaultValue:'',description:'Are you willing in placement side?')
         choice(name: 'Area_Of_Interest', choices: ['Coding','Designing','Others'],description:'Enter Your Area Of Interest')
     }
     stages {
